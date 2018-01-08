0e0e673a34d945204ccf206d59e4f4eec34b7aa9
Ramadhan <mabuak@live.com>
2018-01-08 10:06:39 +0700

    Update MWSClient.php

--- src/MWSClient.php
+++ src/MWSClient.php
@@ -379,7 +379,7 @@ class MWSClient{
      */
     public function ListOrders(DateTime $from, $allMarketplaces = false, $states = [
         'Unshipped', 'PartiallyShipped'
-    ], $FulfillmentChannel = 'MFN'
+    ], $FulfillmentChannel = 'MFN')
     {
         $query = [
             'CreatedAfter' => gmdate(self::DATE_FORMAT, $from->getTimestamp())
