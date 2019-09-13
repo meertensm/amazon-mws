27f5b22bd486e38116e5371a45471b814f3c2835
maioradv <48984465+maioradv@users.noreply.github.com>
2019-09-13 09:56:34 +0200

    Update MWSClient.php

--- src/MWSClient.php
+++ src/MWSClient.php
@@ -41,7 +41,8 @@ class MWSClient{
         'A1VC38T7YXB528' => 'mws.amazonservices.jp',
         'AAHKV2X7AFYLW' => 'mws.amazonservices.com.cn',
         'A39IBJ37TRP1C6' => 'mws.amazonservices.com.au',
-	'A2Q3Y263D00KWC' => 'mws.amazonservices.com'
+	'A2Q3Y263D00KWC' => 'mws.amazonservices.com',
+	'A33AVAJ2PDY3EV' => 'mws-eu.amazonservices.com'
     ];
 
     protected $debugNextFeed = false;
