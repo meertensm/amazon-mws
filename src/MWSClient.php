cc6d57f805caff9d43e4f12f254ee0faccdd102b
maioradv <48984465+maioradv@users.noreply.github.com>
2020-03-23 10:00:28 +0100

    validate credential

--- src/MWSClient.php
+++ src/MWSClient.php
@@ -94,7 +94,7 @@ class MWSClient{
         try{
             $this->ListOrderItems('validate');
         } catch(Exception $e) {
-            if ($e->getMessage() == 'Invalid AmazonOrderId: validate') {
+            if ($e->getMessage() == 'The order id you have requested is not valid') {
                 return true;
             } else {
                 return false;
