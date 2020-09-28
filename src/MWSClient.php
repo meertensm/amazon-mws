2a285dc2f5247376e3c24449eb7c5b0dff4e1707
Tariq Mehmood Khatri <tariq.mehmood@gaditek.com>
2020-09-28 19:05:43 +0500

    Fix - GetCompetitivePricingForASIN returns Competitive Pricing as array some times depending on types of condition of item - New, Used

--- src/MWSClient.php
+++ src/MWSClient.php
@@ -138,8 +138,8 @@ class MWSClient{
 
         $array = [];
         foreach ($response as $product) {
-            if (isset($product['Product']['CompetitivePricing']['CompetitivePrices']['CompetitivePrice']['Price'])) {
-                $array[$product['Product']['Identifiers']['MarketplaceASIN']['ASIN']] = $product['Product']['CompetitivePricing']['CompetitivePrices']['CompetitivePrice']['Price'];
+            if (isset($product['Product']['CompetitivePricing']['CompetitivePrices']['CompetitivePrice'])) {
+                $array[$product['Product']['Identifiers']['MarketplaceASIN']['ASIN']] = $product['Product']['CompetitivePricing']['CompetitivePrices']['CompetitivePrice'];
             }
         }
         return $array;
